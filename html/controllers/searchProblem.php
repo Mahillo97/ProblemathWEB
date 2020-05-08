3b61c0c4a590af593ab79105c11a014af4f04329
Alejandro <mahi-97@hotmail.com>
2020-05-08 11:07:44 +0000

    SearchProblems started

--- html/controllers/searchProblem.php
+++ html/controllers/searchProblem.php
@@ -1,11 +1,11 @@
 
 <?php
 
-class Main extends Controller{
+class SearchProblems extends Controller{
 
     function __construct(){
         parent::__construct();
-        $this->view->render('main/index');
+        $this->view->render('searchProblems/index');
     }
 
     function saludo(){
---
+++ html/views/searchProblems/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" lang="es-ES">
+    <title>Problemath</title>
+    <meta name="description" content="Aplicación de búsqueda de problemas de matemáticas; Universidad de La Rioja." lang="es-ES">
+    <meta name="keywords" content="problemas" lang="es-ES">
+    <meta name="language" content="es-ES">
+    <meta name="robots" content="index,follow">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+
+    <link rel="icon" type="image/png" href="public/img/favicon.png">
+
+</head>
+
+<body>
+    <h1>PROBLEM LIST</h1>
+    <ul class="resultBusqueda">
+
+        <li class="list problema">
+            <!-- <div class="foto">
+                                    <a href="<c:url value='FichaArticulo'>
+                                           <c:param name="cart" value="${art.codigo}"></c:param>
+                                       </c:url>">
+                                        <img src="img/fotosElectr/${art.foto}" alt=${art.codigo} longdesc=${art.nombre} width="80">
+                                    </a>
+                                </div> -->
+            <div class="datos">
+
+                <a href="/ProblemFile?idProblem=1"><strong>Problema 1</strong></a>
+                <div class="proposer">
+                    Oscar Ciaurri
+                </div>
+                <div class="magazine">
+                    Gaceta Matematica
+                </div>
+                <div class="statement">
+                    Sea f(x).... Esto debe de estar en LaTeX
+                </div>
+            </div>
+        </li>
+
+        <li class="list problema">
+            <!-- <div class="foto">
+                                    <a href="<c:url value='FichaArticulo'>
+                                           <c:param name="cart" value="${art.codigo}"></c:param>
+                                       </c:url>">
+                                        <img src="img/fotosElectr/${art.foto}" alt=${art.codigo} longdesc=${art.nombre} width="80">
+                                    </a>
+                                </div> -->
+            <div class="datos">
+
+                <a href="/ProblemFile?idProblem=2"><strong>Problema 2</strong></a>
+                <div class="magazine">
+                    AMS
+                </div>
+                <div class="statement">
+                    Consideremos f(x) una función real .... Esto debe de estar en LaTeX
+                </div>
+            </div>
+        </li>
+
+        <li class="list problema">
+            <!-- <div class="foto">
+                                    <a href="<c:url value='FichaArticulo'>
+                                           <c:param name="cart" value="${art.codigo}"></c:param>
+                                       </c:url>">
+                                        <img src="img/fotosElectr/${art.foto}" alt=${art.codigo} longdesc=${art.nombre} width="80">
+                                    </a>
+                                </div> -->
+            <div class="datos">
+
+                <a href="/ProblemFile?idProblem=5"><strong>Problema 3</strong></a>
+                <div class="proposer">
+                    Manuel Bello
+                </div>
+                <div class="statement">
+                    Consideremos la siguiente desigualdad .... Esto debe de estar en LaTeX
+                </div>
+            </div>
+        </li>
+
+
+    </ul>
+
+</body>
+
+</html>
