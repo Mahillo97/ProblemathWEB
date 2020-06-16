a22ddb57ae7ef9e655f8ed20665f810984ff7b93
Alejandro Mahillo <mahi-97@hotmail.com>
2020-06-16 14:14:25 +0200

    Improved searcher with tags JS

--- html/public/js/addTags.js
+++ html/public/js/addTags.js
@@ -7,47 +7,52 @@ function addTag() {
         if (keyCode == 13 || keyCode == 188) {
             event.preventDefault();
             if (!event.repeat) {
-                var tagname = auxTags.value;
-                if (tagname && !document.getElementById(tagname.trim().toLowerCase())) {
-                    createTag(tagname);
-                    document.fProblems.tags.value = document.fProblems.tags.value + tagname.trim().toLowerCase() + ",";
+                var tagValue = this.value;
+                if (this.value.indexOf(',') > -1) {
+                    tags = this.value.split(',');
+                    for (let tagname of tags) {
+                        createTag(tagname);
+                    }    
+                } else {
+                    createTag(tagValue);
                 }
-                auxTags.value = "";
+                this.value = "";
             }
         }
     });
 }
 
 function createTag(tagname) {
-    var tagList = document.getElementById("tagList");
+    if (tagname && !document.getElementById(tagname.trim().toLowerCase())) {
+        var tagList = document.getElementById("tagList");
 
-    var newTag = document.createElement("div");
-    newTag.id = tagname.trim().toLowerCase();
-    newTag.classList.add("tag", "badge", "badge-danger", "mt-2", "mr-1", "d-flex", "justify-content-between");
+        var newTag = document.createElement("div");
+        newTag.id = tagname.trim().toLowerCase();
+        newTag.classList.add("tag", "badge", "badge-danger", "mt-2", "mr-1", "d-flex", "justify-content-between");
 
-    var divText = document.createElement("div");
-    divText.classList.add("my-auto", "mr-1");
-    divText.innerText = tagname.toLowerCase();
+        var divText = document.createElement("div");
+        divText.classList.add("my-auto", "mr-1");
+        divText.innerText = tagname.toLowerCase();
 
-    var auxDiv = document.createElement("div");
-    var btn = document.createElement("button");
-    btn.classList.add("btn", "btn-danger", "p-0");
-    btn.type = "button";
-    btn.innerHTML = '<i class="far fa-times-circle"></i>'
-    btn.addEventListener("click", function (e) {
-        var deletetag = this.parentNode.parentNode.id;
-        console.log(deletetag)
-        document.fProblems.tags.value = document.fProblems.tags.value.replace(deletetag+',', '')
-        console.log(document.fProblems.tags.value)
-        this.parentNode.parentNode.remove();
-    });
+        var auxDiv = document.createElement("div");
+        var btn = document.createElement("button");
+        btn.classList.add("btn", "btn-danger", "p-0");
+        btn.type = "button";
+        btn.innerHTML = '<i class="far fa-times-circle"></i>'
+        btn.addEventListener("click", function (e) {
+            var deletetag = this.parentNode.parentNode.id;
+            document.fProblems.tags.value = document.fProblems.tags.value.replace(deletetag + ',', '')
+            this.parentNode.parentNode.remove();
+        });
 
-    auxDiv.appendChild(btn);
+        auxDiv.appendChild(btn);
 
-    newTag.appendChild(divText);
-    newTag.appendChild(auxDiv);
+        newTag.appendChild(divText);
+        newTag.appendChild(auxDiv);
 
-    tagList.appendChild(newTag);
+        tagList.appendChild(newTag);
+        document.fProblems.tags.value = document.fProblems.tags.value + tagname.trim().toLowerCase() + ",";
+    }
 }
 
 
